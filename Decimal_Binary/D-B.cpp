d8f901a1cd34ee6a050c5f706d6db9f0a527a0ea
Upkar Kumar Mahto <upkarkumarmahto@Upkars-MacBook-Pro.local>
2023-09-04 17:09:20 +0530

    improved way

--- Decimal_Binary/D-B.cpp
+++ Decimal_Binary/D-B.cpp
@@ -1,19 +1,23 @@
 #include<iostream>
+#import <cmath>
 using namespace std;
 
 int decimaltobinary_Method1(int n){
     //Division Method
+    int binno =0;
+    int i = 0;
     while(n>0){
         int bit = n%2;
-        cout<<bit<<" "<<endl;
+        binno = bit * pow(10,i++) + binno;
         n/=2;
     }
-    return 0;
+    return binno;
 }
 int main()
 {
     int n;
     cin>>n;
     int binary = decimaltobinary_Method1(n);
+    cout<<binary;
     return 0;
 }
