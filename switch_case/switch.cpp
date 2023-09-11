a9c2cbe248b4839010d0dfa60594121e4fa4b03b
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-12 00:20:13 +0530

    Switch case

---
+++ switch_case/switch.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    cout<<"make choices :"<<endl;
+    cout<<"\n0.Addition\t1.Subtraction \t2.Multiplication\t3.Division\n";
+
+    int ch;
+    cin>>ch;
+
+    switch(ch){
+        case 0:
+        cout<<(a+b)<<endl;
+        break;
+
+        case 1:
+        cout<<(a-b);
+        break;
+
+        case 2:
+        cout<<(a*b) ;
+        break;
+
+        case 3:
+        cout<<a/b;
+        break;
+
+        default:
+        cout<<"invalid operation";
+        return 0;
+    }
+
+    
+
+    // if(ch==0){
+    //     cout<<a+b<<" ";
+    // }
+    // else if(ch==1){
+    //     cout<<a-b<<" "<<endl;
+    // }
+
+    // else if(ch==2){
+    //     cout<<a*b<<" " <<endl;
+    // }
+
+    // else if(ch==3){
+    //     cout<<a/b<<" "<<endl;
+    // }
+
+    // else{
+    //     cout<<"Invalid choice"<<endl;
+    //     return 0;
+    // }
+    return 0;
+}
