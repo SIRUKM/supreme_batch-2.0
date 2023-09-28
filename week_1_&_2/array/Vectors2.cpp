a5ebc8f7e8e978d19e4a16e1a7bdf7f5cc3bc6bf
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-09-28 09:42:15 +0530

    vectors

---
+++ week_1_&_2/array/Vectors2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main()
+{
+    vector<int> v;
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(5);
+    int n = v.size();
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    vector<int>::iterator it;
+    for (it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
+
+    for (auto element : v)
+    {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    v.pop_back();
+
+    for (auto element : v)
+    {
+        cout << element << " ";
+    }
+    cout << endl;
+    vector<int> v2(3, 50);
+
+    swap(v, v2);
+
+    for (auto element : v)
+    {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    for (auto element : v2)
+    {
+        cout << element << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
---
+++ week_1_&_2/array/tempCodeRunnerFile.cpp
@@ -0,0 +1,12 @@
+for (int i = 0; i<n; i++)
+    {
+        cout << v[i] << " ";
+    }
+    cout << endl;
+
+    vector<int>::iterator it;
+    for (it = v.begin(); it != v.end(); it++)
+    {
+        cout << *it << " ";
+    }
+    cout << endl;
