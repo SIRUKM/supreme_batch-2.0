517b6401ffd2f93ef50e91c08aafe6f8e28b141b
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2023-11-13 15:06:46 +0530

    pointer2

--- week_6_pointes/pointer.cpp
+++ week_6_pointes/pointer.cpp
@@ -9,8 +9,26 @@ int main()
 
     cout << &a << endl;
 
-    cout << "ptr: " << ptr << endl;
+     cout << "ptr: " << ptr << endl;
 
     cout << "Accessing: " << *ptr << endl;
+
+    cout << "ptr ka address:" << &ptr << endl;
+
+    cout << sizeof(ptr) << endl; // pointer ke andar hamseha he address store hotea hai , so size is always 8.
+
+    char ch = 'a';
+    cout << "ch:" << ch << endl;
+
+    char *chPtr = &ch;
+
+    cout << "Addrerss ch: " << &ch << endl;
+
+    cout << "Address :" << chPtr << endl;
+
+    cout << "Derefrencing :" << *chPtr << endl;// * se hamesha he derefrence hota hia
+
+    cout << sizeof(chPtr) << endl;
+
     return 0;
 }
