684a5f674e50078142a853ccab8e343c61ab1f7d
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-05-26 23:25:12 +0530

    Refactor const_keyword.cpp file

---
+++ week9_OOps/const.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    const int  a = 10;
+    // a = 20; // Error: assignment of read-only variable 'a'
+    return 0;
+}
--- week9_OOps/cosnt_keyword.cpp
+++ week9_OOps/cosnt_keyword.cpp
@@ -2,8 +2,27 @@
 using namespace std;
 int main()
 {
-    const int x = 4;
-    cout<<x<<endl;
+    // const int x = 4;
+    // cout<<x<<endl;
+
+//     const int *a = new int(2);
+//     cout<<*a<<endl;
+//     int b = 20;
+//     // a = &b;
+//     // cout<<*a<<endl;
+
+// a = &b;
+// cout<<*a<<endl;
     
+
+
+    // int * const a = new int(2);
+    // cout<<*a<<endl;
+    // *a = 20;
+    // cout<<*a<<endl;
+
+
+    
+
     return 0;
 }
