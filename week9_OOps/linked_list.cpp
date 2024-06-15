83458ed5bea026454103aa3dcc76be7b4ea50db2
Upkar Kumar Mahto <upkarmahto48@gmail.com>
2024-06-15 15:44:53 +0530

    deletion ll

--- week9_OOps/linked_list.cpp
+++ week9_OOps/linked_list.cpp
@@ -30,7 +30,7 @@ void printLinkedList(Node *head)
     }
     cout << "NULL" << endl;
 }
-void printLength(Node *head)
+int printLength(Node *head)
 {
     Node *temp = head;
     int count = 0;
@@ -39,7 +39,7 @@ void printLength(Node *head)
         count++;
         temp = temp->next;
     }
-    cout << "Length of linked list is: " << count << endl;
+    return count;
 }
 
 void insertAtHead(Node *&head, Node *&tail, int data)
@@ -55,32 +55,131 @@ void insertAtHead(Node *&head, Node *&tail, int data)
         Node *newNode = new Node(data);
         newNode->next = head;
         head = newNode;
-        }
+    }
 }
 
-
-void insetAtTail(Node *&tail, int data){
-    Node * newNode = new Node(data);
+void insetAtTail(Node *&tail, int data)
+{
+    Node *newNode = new Node(data);
     tail->next = newNode;
     tail = newNode;
 }
+
+void insertAtPosition(Node *&head, Node *&tail, int data, int position)
+{
+    if (position < 1)
+    {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    int length = 0;
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        length++;
+        temp = temp->next;
+    }
+    // Rest of the code...
+
+    if (position > length)
+    {
+        cout << "Invalid position" << endl;
+        return;
+    }
+    if (position == 1)
+    {
+        insertAtHead(head, tail, data);
+    }
+    else if (position == length)
+    {
+        insetAtTail(tail, data);
+    }
+
+    else
+    {
+        // insert at the middle
+        Node *temp = new Node(data);
+        Node *current = head;
+        for (int i = 1; i < position - 1; i++)
+        {
+            current = current->next;
+        }
+        temp->next = current->next;
+        current->next = temp;
+    }
+}
+
+void deleteNode(Node *&head, Node * &tail, int position)
+{
+    if (head == NULL)
+    {
+        cout << "Linked list is empty" << endl;
+        return;
+    }
+    int len = printLength(head);
+    if (position == 1)
+    {
+        Node *temp = head;
+        head = head->next;
+        temp->next = NULL;
+        delete temp;
+    }
+
+    else if (position == len)
+    {
+        Node * prev = head;
+        while(prev->next != tail){
+            prev = prev->next;
+        }
+        prev->next = NULL;
+        delete tail;
+        tail = prev;
+
+    }
+    else
+    {
+        Node * prev = NULL;
+        Node * current = head;
+        while(position > 1){
+            prev = current;
+            current = current->next;
+            position--;
+        }
+
+        prev->next = current->next;
+        current->next = NULL;
+        delete current;
+
+    }
+}
 int main()
 {
-    Node *first = new Node(10);
-    Node *second = new Node(20);
-    Node *third = new Node(30);
-    Node *fourth = new Node(40);
+    // Node *first = new Node(10);
+    // Node *second = new Node(20);
+    // Node *third = new Node(30);
+    // Node *fourth = new Node(40);
 
-    Node *head = first;
-    Node *tail = fourth; // Declare the 'tail' variable
-    first->next = second;
-    second->next = third;
-    third->next = fourth;
+    // Node *head = first;
+    // Node *tail = fourth; // Declare the 'tail' variable
+    // first->next = second;
+    // second->next = third;
+    // third->next = fourth;
+    // printLinkedList(head);
+    // // printLength(head);
+    // insertAtHead(head, tail, 5);
+    // // printLength(head);
+    // insetAtTail(tail, 50);
+    // printLinkedList(head);
+
+    Node *head = NULL;
+    Node *tail = NULL;
+    insertAtHead(head, tail, 10);
+    insertAtHead(head, tail, 20);
+    insertAtHead(head, tail, 30);
+    insertAtHead(head, tail, 40);
+    insertAtHead(head, tail, 50);
     printLinkedList(head);
-    // printLength(head);
-    insertAtHead(head, tail, 5);
-    // printLength(head);
-    insetAtTail(tail, 50);
+    insertAtPosition(head, tail, 100, 3);
     printLinkedList(head);
 
     return 0;
